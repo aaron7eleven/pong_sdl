2a29fd8cd3cc7ac0fc52b7ea9b6ce41ebda951fb
Aaron Arenas <aatartanfb@gmail.com>
2023-09-04 19:15:00 -0700

    syncing

--- appSettings.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SDL.h>
-
-struct appSettings {
-	int screenWidth = 1280;
-	int screenHeight = 720;
-	int screenFPS = 60;
-	int screenTicksPerFrame = 1000 / screenFPS; // 1000 ms / X frames
-};
