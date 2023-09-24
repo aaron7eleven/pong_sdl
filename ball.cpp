cec6d54d27b91832951c5c5fe0fafd3afe9adcea
Aaron Arenas <aatartanfb@gmail.com>
2023-09-23 23:05:01 -0700

    added way to render circle

--- ball.cpp
+++ ball.cpp
@@ -23,24 +23,150 @@ void update(float deltaTime, inputs* inputs, ball* ball) {
 }
 
 void renderCircle(SDL_Renderer* renderer, SDL_FPoint center, float radius) {
-	// https://saturncloud.io/blog/midpoint-circle-algorithm-for-filled-circles/#:~:text=The%20midpoint%20circle%20algorithm%20is,midpoint%20between%20two%20adjacent%20points.
-	// Decision Parameter (p)
-	float p = 1.0f - radius;
-	SDL_Point point = { center.x, center.y - radius };
+	// https://en.wikipedia.org/w/index.php?title=Midpoint_circle_algorithm&oldid=889172082#C_example
+	SDL_FPoint point = { radius - 1.0f, 0.0f };
+	SDL_FPoint diff = { 1.0f, 1.0f };
+	float error = diff.x - (radius * 2.0f);
 
 	while (point.x >= point.y) {
-		if (p < 0) {
-			point.x++;
-			p += (2.0f * point.x) + 1.0f;
-		}
+		// Draws circle outline
+				// Draws circle outline
+		// Octant 1 - above +x axis
+		SDL_RenderDrawPointF(renderer, center.x + point.x, center.y + point.y);
+		// Octant 2 (Mirror of Octant 1) - right +y axis
+		SDL_RenderDrawPointF(renderer, center.x + point.y, center.y + point.x);
+		// Octant 3 - above -x axis
+		SDL_RenderDrawPointF(renderer, center.x - point.x, center.y + point.y);
+		// Octant 4 (Mirror of Octant 3) - left +y axis
+		SDL_RenderDrawPointF(renderer, center.x - point.y, center.y + point.x);
+		// Octant 5 - below -x axis
+		SDL_RenderDrawPointF(renderer, center.x - point.x, center.y - point.y);
+		// Octant 6 (Mirror of Octant 5) - left -y axis
+		SDL_RenderDrawPointF(renderer, center.x - point.y, center.y - point.x);
+		// Octant 7 - below +x axis
+		SDL_RenderDrawPointF(renderer, center.x + point.x, center.y - point.y);
+		// Octant 8 (Mirror of Octant 7) - right -y axis
+		SDL_RenderDrawPointF(renderer, center.x + point.y, center.y - point.x);
 
-		if (p >= 0) {
-			point.x++;
+		if (error <= 0.0f) {
 			point.y++;
-			p += (2.0f * point.x) - (2.0f * point.y) + 1.0f;
+			error += diff.y;
+			diff.y += 2.0f;
 		}
 
-		SDL_RenderDrawPointF(renderer, point.x, point.y);
+		if (error > 0.0f) {
+			point.x--;
+			diff.x += 2.0f;
+			error += diff.x - (radius * 2.0f);
+		}
+	}
+}
+
+void renderFillCircle(SDL_Renderer* renderer, SDL_FPoint center, float radius) {
+	// https://en.wikipedia.org/w/index.php?title=Midpoint_circle_algorithm&oldid=889172082#C_example
+	SDL_FPoint point = { radius - 1.0f, 0.0f };
+	SDL_FPoint diff = { 1.0f, 1.0f };
+	float error = diff.x - (radius * 2.0f);
+	SDL_FPoint oct1 = { 0.0f };
+	SDL_FPoint oct2 = { 0.0f };
+	SDL_FPoint oct3 = { 0.0f };
+	SDL_FPoint oct4 = { 0.0f };
+	SDL_FPoint oct5 = { 0.0f };
+	SDL_FPoint oct6 = { 0.0f };
+	SDL_FPoint oct7 = { 0.0f };
+	SDL_FPoint oct8 = { 0.0f };
+
+	while (point.x >= point.y) {
+		// Draws circle outline
+		//// Octant 1 - above +x axis
+		//SDL_RenderDrawPointF(renderer, center.x + point.x, center.y + point.y);
+		//// Octant 2 (Mirror of Octant 1) - right +y axis
+		//SDL_RenderDrawPointF(renderer, center.x + point.y, center.y + point.x);
+		//// Octant 3 - above -x axis
+		//SDL_RenderDrawPointF(renderer, center.x - point.x, center.y + point.y);
+		//// Octant 4 (Mirror of Octant 3) - left +y axis
+		//SDL_RenderDrawPointF(renderer, center.x - point.y, center.y + point.x);
+		//// Octant 5 - below -x axis
+		//SDL_RenderDrawPointF(renderer, center.x - point.x, center.y - point.y);
+		//// Octant 6 (Mirror of Octant 5) - left -y axis
+		//SDL_RenderDrawPointF(renderer, center.x - point.y, center.y - point.x);
+		//// Octant 7 - below +x axis
+		//SDL_RenderDrawPointF(renderer, center.x + point.x, center.y - point.y);
+		//// Octant 8 (Mirror of Octant 7) - right -y axis
+		//SDL_RenderDrawPointF(renderer, center.x + point.y, center.y - point.x);
+
+		oct1.x = center.x + point.x;
+		oct1.y = center.y + point.y;
+
+		oct2.x = center.x + point.y;
+		oct2.y = center.y + point.x;
+
+		oct3.x = center.x - point.x;
+		oct3.y = center.y + point.y;
+
+		oct4.x = center.x - point.y;
+		oct4.y = center.y + point.x;
+
+		oct5.x = center.x - point.x;
+		oct5.y = center.y - point.y;
+
+		oct6.x = center.x - point.y;
+		oct6.y = center.y - point.x;
+
+		oct7.x = center.x + point.x;
+		oct7.y = center.y - point.y;
+
+		oct8.x = center.x + point.y;
+		oct8.y = center.y - point.x;
+
+		// xy axis are using traditional axis orientations. NOT SDL's!!! (y is flipped)
+		// Octant 1 - above +x axis
+		SDL_RenderDrawPointF(renderer, oct1.x, oct1.y);
+		// Octant 2 (Mirror of Octant 1) - right +y axis
+		SDL_RenderDrawPointF(renderer, oct2.x, oct2.y);
+		// Octant 3 - above -x axis
+		SDL_RenderDrawPointF(renderer, oct3.x, oct3.y);
+		// Octant 4 (Mirror of Octant 3) - left +y axis
+		SDL_RenderDrawPointF(renderer, oct4.x, oct4.y);
+		// Octant 5 - below -x axis
+		SDL_RenderDrawPointF(renderer, oct5.x, oct5.y);
+		// Octant 6 (Mirror of Octant 5) - left -y axis
+		SDL_RenderDrawPointF(renderer, oct6.x, oct6.y);
+		// Octant 7 - below +x axis
+		SDL_RenderDrawPointF(renderer, oct7.x, oct7.y);
+		// Octant 8 (Mirror of Octant 7) - right -y axis
+		SDL_RenderDrawPointF(renderer, oct8.x, oct8.y);
+		
+		// To fill circles, draw line between across circle horizonally or vertically
+		// Choosing horizontally -> easier to think about and how pixels are actually drawn
+		// Need to connect mirror across y-axis from left to right
+		// Ex:
+		// - Octant 3 to Octant 1 (Non-Mirrored Octants)
+		// - Octant 4 to Octant 2 (Mirrored Octants)
+		// Could also so a semi circle, portion of circle based on angle or adjustable outline circle
+		
+		// Draws bottom half of circle (y axis is flip in SDL [0 (or -y) is at top of window, and +y is at the bottom of window])
+		//SDL_SetRenderDrawColor(renderer, magenta);
+		SDL_RenderDrawLine(renderer, oct3.x, oct3.y, oct1.x, oct1.y);
+		SDL_RenderDrawLine(renderer, oct4.x, oct4.y, oct2.x, oct2.y);
+		
+		// Draws top half
+		//SDL_SetRenderDrawColor(renderer, cyan);
+		SDL_RenderDrawLine(renderer, oct5.x, oct5.y, oct7.x, oct7.y);
+		SDL_RenderDrawLine(renderer, oct6.x, oct6.y, oct8.x, oct8.y);
+		//SDL_SetRenderDrawColor(renderer, white);
+
+		if (error <= 0.0f) {
+			point.y++;
+			error += diff.y;
+			diff.y += 2.0f;
+		}
+
+		if (error > 0.0f) {
+			point.x--;
+			diff.x += 2.0f;
+			error += diff.x - (radius * 2.0f);
+		}
 	}
 }
 
@@ -48,8 +174,19 @@ void render(SDL_Renderer* renderer, ball* ball) {
 	// Draw Ball
 	SDL_SetRenderDrawColor(renderer, ball->color);
 
-	renderCircle(renderer, ball->circleCollider.center, ball->circleCollider.radius);
+	//renderCircle(renderer, ball->circleCollider.center, ball->circleCollider.radius);
+	renderFillCircle(renderer, ball->circleCollider.center, ball->circleCollider.radius);
+	// Debug
+	//SDL_FRect debug = {
+	//	ball->circleCollider.center.x,
+	//	ball->circleCollider.center.y,
+	//	ball->circleCollider.radius,
+	//	ball->circleCollider.radius
+	//};
 
+	//circleColor() // available in SDL_gfx
+	//SDL_SetRenderDrawColor(renderer, yellow);
+	//SDL_RenderFillRectF(renderer, &debug);
 	//SDL_RenderFillRectF(renderer, ball->circleCollider);
 	//SDL_RenderFillCircle(renderer, ball.circleCollider.center.x, ball.circleCollider.center.y, ball.circleCollider.radius);
 
